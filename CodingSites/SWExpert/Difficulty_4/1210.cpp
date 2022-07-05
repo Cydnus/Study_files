9b2a033fb4fc2991d5e30131de9c39bda9d39d27
Cydnus <53419752+Cydnus@users.noreply.github.com>
2022-07-05 20:48:42 +0900

    220705 Update

---
+++ CodingSites/SWExpert/Difficulty_4/1210.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+#define MAX_MAP_SIZE    100
+
+using namespace std;
+
+int bottomUp(vector<vector<int>> ladderMap, int row, int col)
+{
+    if( row == 0)
+        return col;
+
+    ladderMap[row][col] = 3;
+    if(col+1 < MAX_MAP_SIZE && ladderMap[row][col+1] == 1)
+        return bottomUp(ladderMap, row, col+1);
+    if(col-1 > -1 && ladderMap[row][col-1] == 1)
+        return bottomUp(ladderMap,row,col-1);
+    return bottomUp(ladderMap,row-1,col);
+}
+
+int main()
+{
+    
+    freopen("not_input_1210.txt", "r", stdin);
+
+    int T = 10;
+    
+    for(int testCase = 1; testCase<=T; testCase ++)
+    {
+        int testNo;
+        cin>>testNo;
+
+        vector<vector<int>> ladderMap(MAX_MAP_SIZE,vector<int>(MAX_MAP_SIZE,0));
+
+        int end_x = 0;
+
+        for(int i =0 ; i<MAX_MAP_SIZE; i++)
+        {
+            for(int j = 0 ; j<MAX_MAP_SIZE; j++)
+            {
+                cin>>ladderMap[i][j];
+                if( ladderMap[i][j] == 2)
+                    end_x = j;
+            }
+        }
+        
+
+        cout<<"#"<<testCase<<" "<<bottomUp(ladderMap,MAX_MAP_SIZE-1,end_x)<<endl;
+    }
+    return 0;
+}
---
+++ CodingSites/SWExpert/Difficulty_4/1218.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int T=10;
+    
+    for(int testCase = 1; testCase<=T; testCase ++)
+    {
+        int cnt;
+        cin>> cnt;
+        string str;
+        cin>>str;
+        stack<char> st;
+
+        bool check = true;
+
+        for(int i = 0 ; i<cnt; i++)
+        {
+            if( str[i] == '{'||str[i] == '('||str[i] == '['||str[i] == '<')
+                st.push(str[i]);
+            else if(str[i] == '}')
+            {
+                if( st.top() != '{')
+                {
+                    check = false;
+                    break;
+                }        
+                st.pop();
+            }
+            else if(str[i] == ')')
+            {
+                if( st.top() != '(')
+                {
+                    check = false;
+                    break;
+                }            
+                st.pop();            
+            }
+            else if(str[i] == ']')
+            {
+                if( st.top() != '[')
+                {
+                    check = false;
+                    break;
+                }                
+                st.pop();        
+            }
+            else if(str[i] == '>')
+            {
+                if( st.top() != '<')
+                {
+                    check = false;
+                    break;
+                }                
+                st.pop();
+            }
+        }
+
+        cout<<"#"<<testCase<<" ";
+        if(check)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+
+    }
+    return 0;
+}
